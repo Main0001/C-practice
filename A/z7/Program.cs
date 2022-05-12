60317316f49fc06817b5ab1b555f9c0dfc4bdae0
Thund3rStr1ke <rainbowlex311@gmail.com>
2022-05-12 13:36:42 +0300

    UwU

---
+++ A/z7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace z7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            double z1 = 1 - 1 / 4 * Math.Pow(Math.Sin(2 * a), 2) + Math.Cos(2 * a);
+            double z2 = (Math.Pow(Math.Cos(a), 2) + Math.Pow(Math.Cos(a), 4));
+            Console.WriteLine($"z1 = {Math.Round(z1,4)} and z2 = {Math.Round(z2, 4)}");
+        }
+    }
+}
