c9ba281348f84296cca563e29d11efdd44d235bf
Thund3rStr1ke <rainbowlex311@gmail.com>
2022-05-12 11:45:46 +0300

    UwU

---
+++ A/z1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x;
+            int y, z;
+            Console.WriteLine("Enter x value: ");
+            Console.WriteLine("Enter y value: ");
+            x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Rounded value: ");
+            z =
+
+
+        }
+    }
+}
