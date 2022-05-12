a1e89321447c78dbc4c4ba9ed201918c7e1333df
Thund3rStr1ke <rainbowlex311@gmail.com>
2022-05-12 13:24:06 +0300

    UwU

---
+++ A/z6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace z6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0;
+            Console.WriteLine("Enter 4-digit number");
+            number = int.Parse(Console.ReadLine());
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+            Console.WriteLine($"Digits sum: {sum}");
+        }
+    }
+}
