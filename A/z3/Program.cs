8b5549163b624c5a9c8d61aba97f38c4fa6f4acb
Thund3rStr1ke <rainbowlex311@gmail.com>
2022-05-12 12:32:05 +0300

    UwU

---
+++ A/z3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            double result = (Math.Sqrt(x * Math.Sin(2 * x) + Math.Pow(2.718, (-2 * 1))) * (x + Math.Log10(Math.Sqrt(x))));
+            Console.WriteLine($"Result: {Math.Round(result, 4)}");
+        }
+    }
+}
