033d05833c3d011328b1eab95629bf0ac2b26392
Thund3rStr1ke <rainbowlex311@gmail.com>
2022-05-12 12:44:47 +0300

    UwU

---
+++ A/z4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 12, v2 = 8, t1 = 39, t2 = 53, V;
+            float T;
+            Console.WriteLine($"Volume of liquids: {V = v1 + v2}");
+            Console.Write($"Temperature of liquids: {T = (v1 * t1 + v2 * t2) / V}");
+        }
+    }
+}
