439ebd50b5be950537218fb0df88eb5f40462d86
Maxim <makmushinskiy@inbox.ru>
2022-05-13 11:23:56 +0300

    day2

---
+++ day2/Task/z3(T)/Program.cs
@@ -0,0 +1,16 @@
+﻿try
+{
+    Console.Write("Введите k = ");
+    var k = int.Parse(Console.ReadLine());
+    Console.Write("Введите n = ");
+    var n = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine(k);
+    } 
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
