2a398597532a5d699c79ca9516cdad47de24603b
存在の幻想 <rainbowlex311@gmail.com>
2022-05-13 11:57:06 +0300

    💚🈯♻

---
+++ 1/1/z2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3-digit value:");
+            string str = Console.ReadLine();
+            if (str.Substring(0, 1) == str.Substring(1, 1) && str.Substring(1, 1) == str.Substring(1, 1))
+            {
+                Console.WriteLine("digits in number are the same");
+            }
+            else Console.WriteLine("digits are not the same");
+        }
+    }
+}
---
+++ 1/1/z4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a % 2 != 0)
+                a++;
+            for (int i = a; i < b; i += 2)
+                if (i % 5 != 0)
+                    Console.Write($"{i} ");
+        }
+    }
+}
