1522f8add894dcba96eeb54f3491596b86c708b1
存在の幻想 <rainbowlex311@gmail.com>
2022-05-13 10:45:44 +0300

    💚🈯♻

--- 1/1/z1/Program.cs
+++ 1/1/z1/Program.cs
@@ -6,10 +6,24 @@ namespace z1
     {
         static void Main(string[] args)
         {
-            double x;
-            Console.Write("Enter x: ");
-            x = double.Parse(Console.ReadLine());
-            double y;
+            double x, y;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter x value (more or less than 0.5): ");
+                    x = double.Parse(Console.ReadLine());
+                    if (x < 0)
+                    {
+                        throw new Exception();
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Value cannot be a negative number or letter");
+                }
+            }
             if (x < 0.5)
             {
                 y = (Math.Pow(Math.Log10(x), 3) + Math.Pow(x, 2)) / Math.Sqrt(x + 2);
@@ -19,7 +33,7 @@ namespace z1
                 y = Math.Cos(x) + 2 * Math.Pow(Math.Sin(x), 2);
             }
 
-            Console.WriteLine($"y = {y}");
+            Console.WriteLine($"y = {Math.Round(y, 4)}");
         }
     }
 }
