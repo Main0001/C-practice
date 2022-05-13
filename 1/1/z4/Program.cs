f9444e9a5df89439be8b7d91866c8255d0565994
存在の幻想 <rainbowlex311@gmail.com>
2022-05-13 16:51:45 +0300

    💚🈯♻

--- 1/1/z4/Program.cs
+++ 1/1/z4/Program.cs
@@ -6,13 +6,32 @@ namespace z4
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter A value: ");
             int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter B value: ");
             int b = int.Parse(Console.ReadLine());
             if (a % 2 != 0)
                 a++;
             for (int i = a; i < b; i += 2)
                 if (i % 5 != 0)
                     Console.Write($"{i} ");
+                    Console.WriteLine();
+            int c = a;
+            while (c < b)
+            {
+                if (c % 5 != 0 && c % 2 == 0)
+                    Console.Write($"{c} ");
+                c++;
+            }
+            Console.WriteLine();
+            c = a;
+            do
+            {
+                if (c % 5 != 0 && c % 2 == 0)
+                    Console.Write($"{c} ");
+                c++;
+            }
+            while (c < b);
         }
     }
 }
