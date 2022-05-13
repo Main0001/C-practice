473476b134caeec50b85b698cc94a25abc94a036
存在の幻想 <rainbowlex311@gmail.com>
2022-05-13 10:28:37 +0300

    💚🈯✅

---
+++ 1/1/z1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            Console.Write("Enter x: ");
+            x = double.Parse(Console.ReadLine());
+            double y;
+            if (x < 0.5)
+            {
+                y = (Math.Pow(Math.Log10(x), 3) + Math.Pow(x, 2)) / Math.Sqrt(x + 2);
+            }
+            else
+            {
+                y = Math.Cos(x) + 2 * Math.Pow(Math.Sin(x), 2);
+            }
+
+            Console.WriteLine($"y = {y}");
+        }
+    }
+}
