eb0e50a910a8a3c0a990c67b08bceb5d0b331c67
1stNIIICE <maksmarcuta777@gmail.com>
2022-05-13 09:40:48 +0300

    renamed catalog

---
+++ Day 1/z7/Program.cs
@@ -0,0 +1,6 @@
+﻿double a = double.Parse(Console.ReadLine());
+double bettiNumber = double.Parse(Console.ReadLine());
+
+
+double z1 = (Math.Sin(a) + Math.Cos(2 * bettiNumber - a)) / (Math.Cos(a) - Math.Sin(2 * bettiNumber - a));
+double z2 = (1 + Math.Sin(2 * bettiNumber)) / Math.Cos(2 * bettiNumber);
