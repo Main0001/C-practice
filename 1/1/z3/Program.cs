c5e1c97ee3e873b7749d44f67be376a154774037
存在の幻想 <rainbowlex311@gmail.com>
2022-05-13 13:32:13 +0300

    💚🈯♻

--- 1/1/z3/Program.cs
+++ 1/1/z3/Program.cs
@@ -6,7 +6,40 @@ namespace z3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter value: ");
+                    number = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Value is incorrect!");
+                }
+            }
+            switch (number)
+            {
+                case 1:
+                case 2:
+                case 9:
+                    Console.WriteLine("Backetball");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Run");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Barbell");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
