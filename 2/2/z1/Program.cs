bda0a1d8f4623809420d7b328cda4f00cda818f5
存在の幻想 <rainbowlex311@gmail.com>
2022-05-14 14:02:06 +0300

    💛⚜☢

--- 2/2/z1/Program.cs
+++ 2/2/z1/Program.cs
@@ -2,11 +2,33 @@
 
 namespace z1
 {
-    class Program
+    class A
+    {
+        public int a;
+        public int b;
+
+        public A(int a_, int b_)
+        {
+            this.a = a_; this.b = b_;
+        }
+
+        public double ExpressionA()
+        {
+            return (double)((b - 2) / (2 - a));
+        }
+
+        public double ExpressionB()
+        {
+            return (double)(Math.Pow(Math.Sqrt(a), 10));
+        }
+    }
+    class Temp
     {
         static void Main(string[] args)
         {
-
+            A classA = new A(3, 5);
+            Console.WriteLine($"First exression (b-2) / (2-a) = {classA.ExpressionA()}");
+            Console.WriteLine($"Second expression (sqrt in pow 10 from A) = {Math.Round(classA.ExpressionB(), 4)}");
         }
     }
 }
--- 2/2/z2/Program.cs
+++ 2/2/z2/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
-
-namespace z2
+﻿namespace z2
 {
-    class Program
+    class Ship
     {
-        static void Main(string[] args)
-        {
+        public string name;
+        public string material;
+        public string type;
+        public void Move() { }
+    }
 
-        }
+    class Steamer : Ship
+    {
+        public string typeOfSteamEngine;
+        public void RunSteamEngine() { }
+    }
+
+    class Sailboat : Ship
+    {
+        public string typeOfSail;
+        public void SpreadTheSail() { }
+    }
+
+    class Corvette : Ship
+    {
+        public int amountOfGuns;
+        public void Fire() { }
     }
 }
