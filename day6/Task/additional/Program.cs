934b1869fafdd4b3644f5396986d5dfff909881a
Maxim <makmushinskiy@inbox.ru>
2022-05-18 10:49:41 +0300

    day6

---
+++ day6/Task/additional/Program.cs
@@ -0,0 +1,42 @@
+﻿try
+{
+    const int n = 7;
+    const int m = 7;
+    int[,] matrix = new int[n, m];
+
+    int row = 0;
+    int col = 0;
+    int dx = 1;
+    int dy = 0;
+    int dirChanges = 0;
+    int visits = m;
+
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        matrix[row, col] = i + 1; // поочередное заполнение данными
+        if (--visits == 0)
+        {
+            visits = m * (dirChanges % 2) + n * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+            int temp = dx;
+            dx = -dy;
+            dy = temp;
+            dirChanges++;
+        }
+
+        col += dx;
+        row += dy;
+    }
+
+    for (int i = 0; i < n; i++) //вывод
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(matrix[i, j] + "    ");
+        }
+        Console.WriteLine();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
