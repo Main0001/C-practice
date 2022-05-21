6d1095a0a4f75ffb68600d2d3ec2709d0f5dccab
存在の幻想 <rainbowlex311@gmail.com>
2022-05-21 11:13:40 +0300

    💛☣⚜

---
+++ 8/8/z1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Znak> set = new SortedSet<Znak>(new ZnakComparer());
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter surname: ");
+                string surname = Console.ReadLine();
+                Console.Write("Enter Zodiac sign: ");
+                string sign = Console.ReadLine();
+                Console.WriteLine("Enter information about birthday: ");
+                Console.Write("Enter day of birth: ");
+                int day = int.Parse(Console.ReadLine());
+                Console.Write("Enter month of birth: ");
+                int month = int.Parse(Console.ReadLine());
+                Console.Write("Enter year of birth: ");
+                int year = int.Parse(Console.ReadLine());
+
+                int[] bornDate = new int[] { day, month, year };
+                set.Add(new Znak(name, surname, sign, bornDate));
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter Zodiac sign (enter '0' to exit): ");
+                string zodiac = Console.ReadLine();
+
+                List<Znak> result = (from n in set where n.zodiac == zodiac select n).ToList<Znak>();
+                if (result.Count != 0)
+                {
+                    foreach (var n in result)
+                    {
+                        Console.WriteLine(n.ToString());
+                    }
+                }
+                else if (zodiac == "0")
+                {
+                    break;
+                }
+                else { Console.WriteLine($"No data found with sign '{zodiac}''!"); }
+
+            }
+        }
+    }
+    public struct Znak
+    {
+        public string zName;
+        public string zSurname;
+        public string zodiac;
+        public int[] Birth;
+
+
+        public Znak(string name, string surname, string zod, int[] date)
+        {
+            zName = name;
+            zSurname = surname;
+            zodiac = zod;
+            Birth = new int[date.Length];
+            Birth = date;
+        }
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Surname: {1}, Zodiac sign: {2}, Date of birth: {3}.{4}.{5}",
+                zName, zSurname, zodiac, Birth[0], Birth[1], Birth[2]);
+        }
+
+    }
+    class ZnakComparer : IComparer<Znak>
+    {
+        public int Compare(Znak x, Znak y)
+        {
+            if (x.Birth[0] > y.Birth[0])
+                return 1;
+            if (x.Birth[0] < y.Birth[0])
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ 8/8/z2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex2
+{
+    public enum PositionWorkTime : int
+    {
+        CEO = 101,
+        Accountant = 45,
+        Designer = 66,
+        Art_Manager = 69,
+        Admin = 89,
+        Merchandiser = 55,
+        Ceo_Deputy = 90,
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee work time: ");
+            foreach (int i in Enum.GetValues(typeof(PositionWorkTime)))
+                Console.WriteLine($"{Enum.GetName(typeof(PositionWorkTime), i)}, {i}" + " hrs");    
+        }
+    }
+}
