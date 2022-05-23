da24b5e1c074c2914c17388f5aed8c004c380123
存在の幻想 <rainbowlex311@gmail.com>
2022-05-23 12:11:21 +0300

    💤💙💫

---
+++ 9/9/z1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+TestClass testClass = new TestClass();
+TestClass testClass1 = new TestClass(2);
+
+Console.WriteLine($"IxF0 - "); testClass.IxF0(2);
+Console.WriteLine($"IxF1 - "); testClass1.IxF1();
+
+Console.WriteLine("неявная неоднозначная реализация: ");
+testClass.F0(2);
+testClass1.F1();
+
+Console.WriteLine("Явное приведение к типу интерфейса: ");
+(testClass as Iy).F0(3);
+(testClass1 as Iz).F1();
+
+Console.WriteLine("Ix --->");
+Ix ix = testClass1;
+ix.IxF0(2);
+ix.IxF1();
+
+Console.WriteLine("Iy --->");
+Iy iy = testClass1;
+iy.F0(2);
+iy.F1();
+
+Console.WriteLine("Iz --->");
+Iz iz = testClass1;
+iz.F0(2);
+iz.F1();
+
+
+interface Ix
+{
+    void IxF0(int w);
+    void IxF1();
+}
+interface Iy
+{
+    void F0(int w);
+    void F1();
+}
+interface Iz
+{
+    void F0(int w);
+    void F1();
+}
+
+class TestClass : Ix, Iy, Iz
+{
+    int w;
+    public TestClass()
+
+    {
+        w = 63;
+
+    }
+    public TestClass(int W)
+    {
+        w = W;
+    }
+    public void IxF0(int w)
+    {
+        Console.WriteLine(w + 5);
+    }
+
+    public void IxF1()
+    {
+        Console.WriteLine(w + 5);
+    }
+
+    public void F0(int w)
+    {
+        Console.WriteLine(Math.Pow(w, 3));
+    }
+
+    public void F1()
+    {
+        Console.WriteLine(Math.Pow(w, 3));
+    }
+
+    void Iz.F0(int w)
+    {
+        Console.WriteLine(7 * w - 2);
+    }
+
+    void Iz.F1()
+    {
+        Console.WriteLine(7 * w - 2);
+    }
+}
+
---
+++ 9/9/z2/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace new_V4_p4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, M, choose = 1;
+            Console.WriteLine("Enter matrix dimensions (N x M): ");
+            N = int.Parse(Console.ReadLine());
+            M = int.Parse(Console.ReadLine());
+            Matrix matrix = new Matrix(N, M);//Создали матрицу
+
+            Console.WriteLine("fill matrix: ");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    matrix[i, j] = Double.Parse(Console.ReadLine());
+                }
+            }
+
+            while (choose != 5)
+            {
+                Console.WriteLine("Choose operation:\n 1 - Print matrix\n 2 - Print submatrix\n 3 - Change matrix \n 4 - Get value\n 5 - Exit\nEnter number: ");
+                string choice = Console.ReadLine();
+                choose = Convert.ToInt32(choice);
+                switch (choose)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("=====Matrix=====");
+                            matrix.Print();
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("=====Submatrix=====");
+                            Console.WriteLine("Enter coordinates: ");
+                            matrix.Print1();
+                            Console.WriteLine();
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine("Enter dimensions (N x M): ");
+                            N = int.Parse(Console.ReadLine());
+                            M = int.Parse(Console.ReadLine());
+                            matrix = new Matrix(N, M);
+                            Console.WriteLine("Fill matrix: ");
+                            for (int i = 0; i < N; i++)
+                            {
+                                for (int j = 0; j < M; j++)
+                                {
+                                    matrix[i, j] = Double.Parse(Console.ReadLine());
+                                }
+                            }
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            Console.WriteLine("Enter coordinates: ");
+                            N = int.Parse(Console.ReadLine());
+                            M = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"Value: {matrix[N, M]}");
+                            break;
+                        }
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+class Matrix
+{
+    private double[,] matrix;
+    private int N, M;
+
+    public Matrix()
+    {
+        matrix = new Double[0, 0];
+        this.N = 0;
+        this.M = 0;
+    }
+    public Matrix(int N, int M)
+    {
+        matrix = new Double[N, M];
+        this.N = N;
+        this.M = M;
+    }
+
+    public int iN
+    {
+        get
+        {
+            return N;
+        }
+        set
+        {
+            this.N = value;
+        }
+    }
+    public int iM
+    {
+        get
+        {
+            return M;
+        }
+        set
+        {
+            this.M = value;
+        }
+    }
+
+    public double this[int x, int y]
+    {
+
+        set
+        {
+            if (N >= x && M >= y && x >= 0 && y >= 0)
+            {
+                matrix[x, y] = value;
+            }
+            else
+            {
+                Console.WriteLine("Invalid coordinate!");
+                matrix[x, y] = 0;
+            }
+
+        }
+        get
+        {
+            if (N >= x && M >= y && x >= 0 && y >= 0)
+            {
+                return matrix[x-1, y-1];
+            }
+            else
+            {
+                Console.WriteLine("Invalid coordinate!");
+                return 0;
+            }
+        }
+    }
+    public void Print()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < M; j++)
+            {
+                Console.Write(" {0} ", matrix[i, j]);
+            }
+        }
+        Console.WriteLine();
+    }
+    public void Print1()
+    {
+        int r = int.Parse(Console.ReadLine());
+        int s = int.Parse(Console.ReadLine());
+        Console.WriteLine("Submatrix: ");
+        if (r <= N && s <= N)
+        {
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < s; j++)
+                {
+                    Console.Write($" {matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
