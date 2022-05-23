d5ce06379b6d589155bee5986f63e4e422d0808e
Talerchik Alexey <talerchik@protonmail.com>
2022-05-23 10:26:55 +0300

    Day 10

---
+++ Day 10/10-1/Program.cs
@@ -0,0 +1,102 @@
+﻿
+TestClass testClass = new TestClass();
+TestClass testClass1 = new TestClass(5);
+// Эти методы представляют собой неявную ОДНОЗНАЧНУЮ реализацию
+// интерфейса Ix.
+Console.WriteLine($"IxF0 - "); testClass.IxF0(5);
+Console.WriteLine($"IxF1 -"); testClass1.IxF1();
+
+
+// Эти методы представляют собой неявную НЕОДНОЗНАЧНУЮ реализацию
+// интерфейсов Iy и Iz.
+Console.WriteLine("неявную НЕОДНОЗНАЧНУЮ реализацию");
+testClass.F0(5);
+testClass1.F1();
+
+
+// А вот вызов функций с явным приведением к типу интерфейса.
+// Собственный метод класса bF0() при подобных преобразованиях
+// не виден.
+Console.WriteLine("C явным приведением к типу интерфейса");
+(testClass as Iy).F0(7);
+(testClass1 as Iz).F1();
+
+// А теперь настраиваем ссылки различных типов интерфейсов
+// на ОДИН И ТОТ ЖЕ объект-представитель класса TestClass.
+// И через "призму" интерфейса всякий раз объект будет
+// выглядеть по-разному.
+Console.WriteLine("==========Ix==========");
+Ix ix = testClass1;
+ix.IxF0(5);
+ix.IxF1();
+
+Console.WriteLine("==========Iy==========");
+Iy iy = testClass1;
+iy.F0(5);
+iy.F1();
+
+Console.WriteLine("==========Iz==========");
+Iz iz = testClass1;
+iz.F0(5);
+iz.F1();
+
+
+interface Ix
+{
+    void IxF0(double w);
+    void IxF1();
+}
+interface Iy
+{
+    void F0(double w);
+    void F1();
+}
+interface Iz
+{
+    void F0(double w);
+    void F1();
+}
+
+class TestClass : Ix, Iy, Iz
+{
+    double  w;
+    public TestClass()
+
+    {
+        w = 125;
+
+    }
+    public TestClass(double W)
+    {
+        w = W;
+    }
+    public void IxF0(double w)
+    {
+        Console.WriteLine(Math.Log(w));
+    }
+
+    public void IxF1()
+    {
+        Console.WriteLine(Math.Log(w));
+    }
+
+    public void F0(double w)
+    {
+        Console.WriteLine(2 / w);
+    }
+
+    public void F1()
+    {
+        Console.WriteLine(2 / w);
+    }
+
+    void Iz.F0(double w)
+    {
+        Console.WriteLine(w + 2);
+    }
+
+    void Iz.F1()
+    {
+        Console.WriteLine(w + 2);
+    }
+}
---
+++ Day 10/10-2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW11
+{
+    class Stock
+    {
+        List<Product> products = new List<Product>();
+        public Stock(List<Product> porductsList)
+        {
+            this.products = porductsList;
+        }
+        public void WritePlane(int index)
+        {
+            if (index > products.Count - 1)
+                throw new Exception("Продукта не существует");
+            Console.WriteLine("Название продукта: " + products[index].productName);
+            Console.WriteLine("Название магазина: " + products[index].shopName);
+            Console.WriteLine("Стоимость товара в рублях: " + products[index].cost);
+        }
+        public void ProductAdd(Product addProduct)
+        {
+            products.Add(addProduct);
+        }
+        public void AddProductsRange(params Product[] Product)
+        {
+            products.AddRange(Product);
+        }
+    }
+    class Product : IComparable<Product>
+    {
+        private string _productName;
+        private string _shopName;
+        private decimal _cost;
+
+        public Product(string productName, string shopName, decimal cost)
+        {
+            this._productName = productName;
+            this._shopName = shopName;
+            this._cost = cost;
+        }
+
+        public string productName { get { return _productName; } set { _productName = value; } }
+        public string shopName { get { return _shopName; } set { _shopName = value; } }
+        public decimal cost { get { return _cost; } set { _cost = value; } }
+
+        public int CompareTo(Product obj)
+        {
+            if (shopName.CompareTo(obj.shopName)!=0)
+            {
+                return shopName.CompareTo(obj.shopName);
+            }
+            if (productName.CompareTo(obj.productName)!=0)
+            {
+                return productName.CompareTo(obj.productName);
+            }
+            return cost.CompareTo(obj.cost);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("Юбка", "ExPz", 25));
+            products.Add(new Product("Шорты", "ExPу", 55));
+            products.Add(new Product("Юбка", "ExPу", 11));
+            products.Add(new Product("Бита", "ExPzz", 99));
+            products.Sort();
+            Stock product = new Stock(products);
+            for (int i = 0; i < products.Count; i++)
+            {
+                product.WritePlane(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
