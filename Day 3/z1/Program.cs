07d57fce6547cd13be087d828842ab157c554c66
1stNIIICE <66973235+17thNIIICE@users.noreply.github.com>
2022-05-24 03:37:23 +0300

    day 3
    asdfaksdjnfkjansdfijknasdf

---
+++ Day 3/z1/Program.cs
@@ -0,0 +1,24 @@
+﻿A classA = new A(6, 17);
+Console.WriteLine($"Average of a and b is = {classA.GetAvarege()}");
+Console.WriteLine($"b^3+(1/2*a) = {classA.GetResultOfException()}");
+class A
+{
+    public int a;
+    public int b;
+
+    public A(int a_, int b_)
+    {
+        this.a = a_; this.b = b_;
+    }
+
+    public double GetAvarege()
+    {
+        return a + b / 2;
+    }
+
+    public double GetResultOfException()
+    {
+        return Math.Pow(b, 3) + Math.Sqrt(a);
+    }
+}
+
---
+++ Day 3/z2/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Organization
+{
+    public string Name;
+    public Organization(string name)
+    {
+        this.Name = name;
+    }
+}
+public class OilCompany : Organization
+{
+    public int VolumeProduction;
+    public OilCompany(string name, int ListClients, int VolumeProduction)
+        : base(name)
+    {
+        this.VolumeProduction = VolumeProduction;
+    }
+}
+public class Plant : Organization
+{
+    public int NumberWorker;
+    public Plant(string name, int ListClients, int VolumeProduction, int NumberWorker)
+        : base(name)
+    {
+        this.NumberWorker = NumberWorker;
+    }
+}
