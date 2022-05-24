dc911f13b37fcc46957c77d9853f179ea864ca67
1stNIIICE <66973235+17thNIIICE@users.noreply.github.com>
2022-05-24 03:42:35 +0300

    bam bam
    sheesh boom

---
+++ Day 2/z5/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Male = ");
+string male = Console.ReadLine();
+if (male == "M" || male == "m")
+{
+    Console.WriteLine("jack, john, jackson");
+}
+else if (male == "F" || male == "f") Console.WriteLine("Betty, Glory, Kate");
+else Console.WriteLine("None");
+//Console.WriteLine("Enter s");
+//string s = Console.ReadLine();
+//if (s == "S" || s == "s")
+//{
+//    switch (male)
+//    {
+//        case "M":
+//            Console.WriteLine("jack, john, jackson");
+//            break;
+//        case "m":
+//            Console.WriteLine("jack, john, jackson");
+//            break;
+//        case "F":
+//            Console.WriteLine("Betty, Glory, Kate");
+//            break;
+//        case "f":
+//            Console.WriteLine("Betty, Glory, Kate");
+//            break;
+
+//    }
+//}
---
+++ Day 2/z6/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Не бейте пожалуйста я не хочу мучаться и делать отдельные методы для каждлого цикла");
+int price = int.Parse(Console.ReadLine());
+int i = 10;
+int j = 10;
+int z = 10;
+
+Console.WriteLine("While cycle");
+    while (i <= 200)
+    {
+        Console.WriteLine($"Price for {i} count products is {i * price}");
+        i += 10;
+    }
+Console.WriteLine("Do while cycle");
+    do
+    {
+        Console.WriteLine($"Price for {j} count products is {j * price}");
+        j += 10;
+    }
+    while (j <= 200);
+
+Console.WriteLine("For cycle");
+    for( ; z <= 200; z += 10)
+    {
+        Console.WriteLine($"Price for {z} count products is {z * price}");
+    }
+
