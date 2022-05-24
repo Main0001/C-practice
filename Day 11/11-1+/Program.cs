1b75f9b0b569fd7d689d6b19d91d5f131544b693
Talerchik Alexey <talerchik@protonmail.com>
2022-05-24 11:04:40 +0300

    Day 11

---
+++ Day 11/11-1+/Program.cs
@@ -0,0 +1,49 @@
+﻿Magistor student = new Magistor("Talerchik", 9.0, "TP");
+Console.WriteLine(student.AddStipend());
+Console.WriteLine(student.AddStipend(5));
+class Student
+{
+    private string _name;
+    private double _s;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public double s
+    {
+        get { return _s; }
+        set { _s = value; }
+    }
+
+    public double AddStipend()
+    {
+         return 3000000 + 10000 * (_s - 5);
+    }
+
+    public Student(string nameData, double sData) {
+        _name = nameData;
+        _s = sData;
+    }
+}
+
+class Magistor : Student
+{
+    private string _speciality;
+    public string Speciality
+    {
+        get { return _speciality; }
+        set { _speciality = value; }
+    }
+    public Magistor(string nameData, double sData, string speciality)
+    : base(nameData, sData)
+    {
+        Speciality = speciality;
+    }
+    public double AddStipend(int m)
+    {
+        return (3000000 + 10000 * (s - 5)) + m;
+    }
+}
---
+++ Day 11/11-2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10
+{
+    class A
+    {
+        protected int a;
+        protected int b;
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public A()
+        {
+            this.a = default(int);
+            this.b = default(int);
+        }
+    }
+
+    class B : A
+    {
+        private int d;
+        public B(int a, int b, int d)
+            : base(a, b)
+        {
+            this.d = d;
+        }
+        public B(int d)
+        {
+            this.d = d;
+        }
+        public int c2
+        {
+            get
+            {
+
+                var sum = 0;
+                while (a < 10)
+                {
+                    sum += b + d;
+                    a++;
+                }
+                return sum;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B(0, 10, 2); // new B (a, b, d)
+            Console.WriteLine(b.c2);
+            Console.ReadLine();
+        }
+    }
+}
