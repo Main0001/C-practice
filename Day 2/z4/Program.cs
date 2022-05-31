62d8a1dae1816afb7b99566bf52c06e7ff0aa95a
1stNIIICE <66973235+17thNIIICE@users.noreply.github.com>
2022-05-31 13:17:11 +0300

    Update Program.cs

--- Day 2/z4/Program.cs
+++ Day 2/z4/Program.cs
@@ -1,1 +1,11 @@
-﻿
+int x = int.Parse(Console.ReadLine());
+if(x <= 1)
+{
+    double y = 2 * Math.Cos(x) + 3 * Math.Pow(x, 2);
+    Console.WriteLine(y);
+}
+else
+{
+    double y = 2 * Math.Log(x) + Math.Pow(1 + Math.Pow(x, 2), 1/2);
+    Console.WriteLine(y);
+}
