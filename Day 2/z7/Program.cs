6d470194ba30100eaed7b8fb37d0b4afacb9d34d
1stNIIICE <66973235+17thNIIICE@users.noreply.github.com>
2022-05-31 13:54:27 +0300

    Update Program.cs

--- Day 2/z7/Program.cs
+++ Day 2/z7/Program.cs
@@ -1,1 +1,27 @@
-﻿
+string[] maleNames = { "Rufus", "Bear", "Dakota", "Fido",
+                          "Vanya", "Samuel", "Koani", "Volodya",
+                          "Prince", "Yiska" };
+string[] femaleNames = { "Maggie", "Penny", "Saya", "Princess",
+                            "Abby", "Laila", "Sadie", "Olivia",
+                            "Starlight", "Talla" };
+
+string choice = Console.ReadLine();
+
+if (choice ==  "M" || choice == "m")
+{
+    for(int i = 0; i < maleNames.Length; i++)
+    {
+        Console.Write($"{maleNames[i]} ");
+    }
+}
+else if (choice == "F" || choice == "f")
+{
+    for (int i = 0; i < maleNames.Length; i++)
+    {
+        Console.Write($"{femaleNames[i]} ");
+    }
+}
+else
+{
+    Console.WriteLine("None");
+}
