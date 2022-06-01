da9755a5c3771c0ee88cc40acb138f97cc2d93a3
Maxim <makmushinskiy@inbox.ru>
2022-06-01 11:47:37 +0300

    day18

---
+++ day18/day18/FigureLib/Parking/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Parking
+{
+    public class ParkingAuto
+    {
+        public string nameOfAuto;
+        public decimal priceOfAuto;
+        public string typeOfAuto;
+
+        public ParkingAuto(string nameOfAuto, decimal priceOfAuto, string typeOfAuto)
+        {
+            this.nameOfAuto = nameOfAuto;
+            this.priceOfAuto = priceOfAuto;
+            this.typeOfAuto = typeOfAuto;  
+        }
+
+        public string BuyAuto(decimal priceOfAuto)
+        {
+            if (priceOfAuto < this.priceOfAuto)
+            {
+                return "Недостаточно средств";
+            }
+            else
+            {
+                return $"Вы купили машину {nameOfAuto}";
+            }
+        }
+        public string ShowInfo()
+        {
+            return $"Модель - {nameOfAuto}; Цена - {priceOfAuto}; Тип авто - {typeOfAuto};";
+        }
+
+    }
+}
---
+++ day18/day18/test/Program.cs
@@ -0,0 +1,25 @@
+﻿using FigureLib;
+using ClassLibrary1;
+using Parking;
+try
+{
+    Figure figure1 = new Triangle(10, 10, 10);
+    Console.WriteLine("Фигура треугольник создана");
+    //figure1.ShowInfo();
+
+    RectangleFigure figure3 = new Rectangle(5, 5);
+    Console.WriteLine("Фигура прямоугольник создана");
+    //figure3.ShowInfo();
+
+    ParkingAuto auto = new ParkingAuto("Ламборджини", 1000, "Легковой");
+    Console.WriteLine(auto.ShowInfo());
+
+    Console.WriteLine("Введите количество денег для оплаты:");
+    decimal price = decimal.Parse(Console.ReadLine());
+    Console.WriteLine(auto.BuyAuto(price));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.ReadLine();
