a52ee64252b3db6ec56d4fda84ad2eee2409fc56
Talerchik Alexey <talerchik@protonmail.com>
2022-06-02 17:37:57 +0300

    Day 19

---
+++ Day 19/19-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+int a = 2, b = 6;
+Queue<int> fromAToB = new Queue<int>(); //Queue - очередь
+Queue<int> lessThenA = new Queue<int>();
+Queue<int> moreThenB = new Queue<int>();
+int number;
+using (StreamReader reader = new StreamReader(@"file.txt", Encoding.UTF8)) //Открываем поток для чтения файла
+{
+    while (!reader.EndOfStream)
+    {
+        number = int.Parse(reader.ReadLine());//считывание строки, затем перенос на новую строку
+        if (number < a)
+            lessThenA.Enqueue(number); //Enqueue - добавляет элемент в конец очереди
+        else if (number > b)
+            moreThenB.Enqueue(number);
+        else
+            fromAToB.Enqueue(number);
+    }
+}
+Console.WriteLine(string.Join(" ", fromAToB)); //из массива объединяем в строку и добавляем " " между элементами
+Console.WriteLine(string.Join(" ", lessThenA));
+Console.WriteLine(string.Join(" ", moreThenB));
---
+++ Day 19/19-3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.Prototype.Conceptual
+{
+    public class Animal
+    {
+        public int? Age = null;
+        public DateTime? BirthDate = null;
+        public string? Name = null;
+        public IdInfo? IdInfo = null;
+
+        public Animal ShallowCopy()
+        {
+            return (Animal)this.MemberwiseClone();
+        }
+
+        public Animal DeepCopy()
+        {
+            Animal clone = (Animal)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = Name;
+            return clone;
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal p1 = new Animal();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("1977-01-01");
+            p1.Name = "Jack Daniels";
+            p1.IdInfo = new IdInfo(666);
+
+            // Выполнить поверхностное копирование p1 и присвоить её p2.
+            Animal p2 = p1.ShallowCopy();
+            // Сделать глубокую копию p1 и присвоить её p3.
+            Animal p3 = p1.DeepCopy();
+
+            // Вывести значения p1, p2 и p3.
+            Console.WriteLine("Original values of p1, p2, p3:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+
+            // Изменить значение свойств p1 и отобразить значения p1, p2 и p3.
+            p1.Age = 32;
+            p1.BirthDate = Convert.ToDateTime("1900-01-01");
+            p1.Name = "Frank";
+            p1.IdInfo.IdNumber = 7878;
+            Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values (reference values have changed):");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values (everything was kept the same):");
+            DisplayValues(p3);
+
+            var animals = new Dictionary<int, Animal>();
+            animals[p1.IdInfo.IdNumber] = p1;
+            animals[p2.IdInfo.IdNumber] = p2;
+            animals[p3.IdInfo.IdNumber] = p3;
+        }
+
+        public static void DisplayValues(Animal p)
+        {
+            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+                p.Name, p.Age, p.BirthDate);
+            Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
+        }
+    }
+}
