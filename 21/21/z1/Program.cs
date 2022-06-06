47a77dec8ddaa91dbf75824848f47a4d2aa7e4d3
存在の幻想 <rainbowlex311@gmail.com>
2022-06-06 10:57:46 +0300

    🤍◽🗯

---
+++ 21/21/z1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car auto = new Car(4, "Porsche", new GasMove());
+        auto.Move();
+        auto.Movable = new ElectricMove();
+        auto.Move();
+    }
+}
+interface IMovable
+{
+    void Move();
+}
+
+class GasMove : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Uses gasoline to move");
+    }
+}
+
+class ElectricMove : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Uses electricity to move");
+    }
+}
+class Car
+{
+    protected int seats;
+    protected string model;
+
+    public Car(int amount, string model, IMovable move)
+    {
+        this.seats = amount;
+        this.model = model;
+        Movable = move;
+    }
+    public IMovable Movable { private get; set; }
+    public void Move()
+    {
+        Movable.Move();
+    }
+}
