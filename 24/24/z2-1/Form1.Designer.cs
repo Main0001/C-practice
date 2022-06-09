1a1815eb6377d75c39c56d1f6ead056c3aed6f25
存在の幻想 <rainbowlex311@gmail.com>
2022-06-09 15:21:59 +0300

    🥋🤍🤍

---
+++ 24/24/z2-1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _25_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTimer1 = new _25_2_1.UserControlTimer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // userControlTimer1
+            // 
+            this.userControlTimer1.Location = new System.Drawing.Point(6, 19);
+            this.userControlTimer1.Name = "userControlTimer1";
+            this.userControlTimer1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.userControlTimer1.Size = new System.Drawing.Size(109, 92);
+            this.userControlTimer1.TabIndex = 0;
+            this.userControlTimer1.TimeEnabled = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.userControlTimer1);
+            this.groupBox1.Location = new System.Drawing.Point(1, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(166, 157);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Current time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(170, 163);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlTimer userControlTimer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
